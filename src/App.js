4a3f2ce20defb2b913325aa7eaf76512a0ae7ac3
iamtoufiq <iamtoufiq2@gmail.com>
2023-05-15 22:44:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Footer } from "./components/footer/footer";
+const App = () => {
+  return (
+    <div>
+      <h3>Appf</h3>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-div m-up-3 has-acent p-up-2">
+        <div className="textbox">
+          <div className="text is-dark text-center">
+            Made with <span className="is-blue">&lt;/&gt;</span> by Toufiq
+            Choudhari
+          </div>
+          <div className="footer-icons text-center">
+            <a href="https://www.linkedin.com/in/toufiq-choudhari-56208b203/">
+              <button className="btn-icon nav-icons m-x-1">
+                <i className="fab fa-linkedin is-4" />
+              </button>
+            </a>
+            <a href="https://github.com/iamtoufiq">
+              <button className="btn-icon nav-icons m-x-1">
+                <i className="fab fa-github-square is-4" />
+              </button>
+            </a>
+          </div>
+          <div className="text text-center is-2">
+            © {new Date().getFullYear()} Elegance Ui
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export { Footer };
